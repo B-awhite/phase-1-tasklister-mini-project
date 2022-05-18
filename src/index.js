4bdb6fc71dab0ed8c5173bbfc571bca779fe8cd8
Bria White <bria.awhite@gmail.com>
2022-05-18 15:33:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let input = document.querySelector('#new-task-description').value 
+    buildToDo(input)
+    form.reset()
+  })
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p')
+  p.textContent = `${todo} `
+  document.querySelector('#tasks').appendChild(p)
+}
